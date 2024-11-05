dd18d77576f1f65ccfb6e10b4df2befca91c61ee
KudYardo <kimolego52@gmail.com>
2024-11-05 09:47:36 -0500

    Third commit

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 def print_hi(name):
-    print(f'Hello World, from {name}')
+    print(f'Hello World, from {name}!')
 
 
 if __name__ == '__main__':
-    print_hi(input())
+    print_hi(input('Enter your username: '))
 
